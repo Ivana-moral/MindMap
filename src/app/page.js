7ba7c3a56e52051ac0e38387471e58893097435b
YonasWorku <Yo.worku@ufl.edu>
2025-02-16 10:08:17 -0500

    Landing Page Complete
    Landing Page finished including the button, routes to Lesson Explorer Page which is initially created

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      Hello World.
+    <div className="center-container">
+      <h1>Bienvenido al MindMap</h1>
+      <Link href= "/lessonexplorer">
+      <button className="mt-6 px-6 py-3 bg-[#66ff00] text-black rounded-lg text-lg font-semibold hover:bg-[#4ccd00] transition duration-300">
+        See Lessons
+      </button>
+      </Link>
     </div>
   );
 }
