81e9b206bd5448c22031ec997210dc28f108a63e
sorkinr <r.sorkin@ufl.edu>
2025-03-30 14:49:53 -0400

    add cors

--- mindmap-backend/app/main.py
+++ mindmap-backend/app/main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI
 from app.routers import user, lesson, auth
 from app.db.models import Base
 from app.db.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
